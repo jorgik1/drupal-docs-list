88aa2e2f33334398c8b7a8518623ad8d98e37ed9
Yurii Stenin <yuriy.ipcom@gmail.com>
2023-03-20 19:15:40 +0200

    changed callback names.

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Pagination = (props: { onClick: () => void, page: number, onClick1: () => void, lastPage: number }) => {
+const Pagination = (props: { onClickPrev: () => void, page: number, onClickNext: () => void, lastPage: number }) => {
     return <div className="btn-group grid grid-cols-2 space-between justify-end">
         <button
             className="btn text-white px-4 py-2 rounded-md mx-2"
-            onClick={props.onClick}
+            onClick={props.onClickPrev}
             disabled={props.page === 0}
         >
             Previous
         </button>
         <button
             className="btn text-white px-4 py-2 rounded-md mx-2"
-            onClick={props.onClick1}
+            onClick={props.onClickNext}
             disabled={props.page >= props.lastPage}
         >
             Next
